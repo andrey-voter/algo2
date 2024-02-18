2e4e7dd10201824242e67e38c2aed215c78dddd2
Andrey <a.v0dyany@yandex.ru>
2024-02-18 15:21:14 +0300

    Add Maximum_period task; Add .clang-tidy and .clang-formatl; Modify CMakeLists

---
+++ contest_1/Maximum_period.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+void SetUp() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+
+std::vector<int> ZFunc(const std::string &str) {
+    int len = static_cast<int>(str.length());
+    std::vector<int> zed(len);
+    zed[0] = len;
+    int left = 0;
+    int right = 0;
+
+    for (int index = 1; index < len; ++index) {
+        if (index <= right) {
+            zed[index] = std::min(right - index + 1, zed[index - left]);
+        }
+        while (index + zed[index] < len and str[index + zed[index]] == str[zed[index]]) {
+            ++zed[index];
+        }
+        if (index + zed[index] - 1 > right) {
+            right = index + zed[index] - 1;
+            left = index;
+        }
+    }
+    return zed;
+}
+
+int FindMaximumPeriod(const std::string &str) {
+    auto zed = ZFunc(str);
+    int ans = 1;
+    int len = static_cast<int>(zed.size());
+    for (int i = 1; i < len; ++i) {
+        if (zed[i] == len - i and len % i == 0) {
+            ans = len / i;
+            break;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    SetUp();
+    std::string str;
+    std::getline(std::cin, str);
+    std::cout << FindMaximumPeriod(str) << std::endl;
+}
