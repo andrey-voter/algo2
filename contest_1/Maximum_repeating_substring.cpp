61cebfbedf3d8e2f3b7700d058365e76ecd53992
Andrey <a.v0dyany@yandex.ru>
2024-02-19 23:36:44 +0300

    Add Maximum_repeating_substring task

--- contest_1/Maximum_repeating_substring.cpp
+++ contest_1/Maximum_repeating_substring.cpp
@@ -30,27 +30,14 @@ std::vector<int> ZFunc(const std::string &str) {
     return zed;
 }
 
-int FindMaximumPeriod(const std::string &str) {
-    auto zed = ZFunc(str);
-    int ans = 1;
-    int len = static_cast<int>(zed.size());
-    for (int i = 1; i < len; ++i) {
-        if (zed[i] == len - i and len % i == 0) {
-            ans = len / i;
-            break;
-        }
-    }
-    return ans;
-}
-
 int FindMaximumRepeatingSubstring(const std::string &str) {
     int ans = 1;
     int len = static_cast<int>(str.length());
-    for (int begin = 0; begin < len; ++begin) {
-        for (int cur_len = 1; cur_len < len - begin; ++cur_len) {
-            int cur_val = FindMaximumPeriod(str.substr(begin, cur_len));
-            if (cur_val > ans) {
-                ans = cur_val;
+    for (int offset = 0; offset < len; ++offset) {
+        auto zed = ZFunc(str.substr(offset));
+        for (int i = 1; i < len - offset; ++i) {
+            if (zed[i] % i == 0) {
+                ans = std::max(ans, zed[i] / i + 1);
             }
         }
     }
